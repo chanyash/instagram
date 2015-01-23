154d31082eafdcec93ebf1ec8a859903a0642072
Joanna Chan <chanyash@yahoo-inc.com>
2015-01-22 18:54:42 -0800

    Initial Commit

---
+++ instagrame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  instagrame
+//
+//  Created by Joanna Chan on 1/22/15.
+//  Copyright (c) 2015 yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ instagrameTests/instagrameTests.m
@@ -0,0 +1,34 @@
+//
+//  instagrameTests.m
+//  instagrameTests
+//
+//  Created by Joanna Chan on 1/22/15.
+//  Copyright (c) 2015 yahoo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface instagrameTests : XCTestCase
+
+@end
+
+@implementation instagrameTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
